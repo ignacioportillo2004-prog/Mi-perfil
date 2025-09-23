47dfe7dcbe1964184cfc4aad1743d6b761525aee
IgnacioPortillo <ignacioportillo2004@gmail.com>
2025-09-23 18:39:26 -0300

    creando el rpositorio

---
+++ dia 1/main.js
@@ -0,0 +1,26 @@
+let numero = 0
+console.log(numero);
+if (numero<0)
+{
+    console.log("el numero es negativo ");
+    else if(numero>0)
+{
+    console.log("el numero es positivo"); 
+}
+    else {
+        console.log("el numero es cero")
+    
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
